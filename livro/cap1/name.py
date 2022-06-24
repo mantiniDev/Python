5b289b9d35d12dcf7c7ef59f07dacf55d36072b1
mantiniDev <pmantinidev@gmail.com>
2022-06-24 00:08:13 -0300

    Livro_Python

---
+++ livro/cap1/name.py
@@ -0,0 +1,5 @@
+name = "Ada Lovelace"
+print(name.title()) #primeiras palavras com maiscula
+
+print(name.upper()) #deixa a variável toda maiscula
+print(name.lower()) #deixa tudo minusculo.
---
+++ livro/cap1/variaveis.py
@@ -0,0 +1,7 @@
+mensagem = "Hello Python world"
+print(mensagem)
+
+
+mensagem = "Hello Paulo"
+print(mensagem)
+
