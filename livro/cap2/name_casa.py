00dd4383dd1eedcc0080918537cbc46c0550bc71
mantiniDev <pmantinidev@gmail.com>
2022-06-24 13:15:24 -0300

    exercicio_finalizado

--- livro/cap2/name_casa.py
+++ livro/cap2/name_casa.py
@@ -1,13 +1,28 @@
-#2.3 
+#2.3  frase simples
 name = "Benjamin, "
 frase = "você gostaria de aprender um pouco de python hoje? "
 
 print(name + "" + frase)
 
-#2.4 
+#2.4  maisculas e minusculas
 name1 = "benjamin"
 print(name.title())
 print(name.lower())
 print(name.upper())
 
-#2.5
+#2.5 citação
+print('Albert Einstein certa vez disse: "Uma pessoa que\nnunca cometeu um erro jamais tentou nada novo"')
+
+#2.6 
+famous_person = "Albert Einstein,"
+message = 'Uma vez disse: "Uma pessoa que\nnunca cometeu um erro jamais tentou nada novo"'
+print(famous_person + " " + message) 
+
+#27
+nome = "      \n\t     paulo \n\n       "
+print(nome)
+
+print(nome.lstrip())
+print(nome.rstrip())
+print(nome.strip())
+
