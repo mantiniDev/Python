b624f7a34438705f0a118ff310c5e79458baaf1b
mantiniDev <pmantinidev@gmail.com>
2022-07-21 16:20:52 -0300

    aprendendo_JSON

---
+++ 1_aprendendoJson/json.py
@@ -0,0 +1,16 @@
+import json
+
+data_JSON =  """
+{
+	"size": "Medium",
+	"price": 15.67,
+	"toppings": ["Mushrooms", "Extra Cheese", "Pepperoni", "Basil"],
+	"client": {
+		"name": "Jane Doe",
+		"phone": "455-344-234",
+		"email": "janedoe@email.com"
+	}
+}
+"""
+data_dict = json.load(data_JSON)
+print(data_dict)
