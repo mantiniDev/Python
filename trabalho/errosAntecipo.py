1185af4e851f6bc5b342c91c513dd2a2457ff411
mantiniDev <pmantinidev@gmail.com>
2022-08-26 21:34:24 -0300

    input em lista

--- trabalho/errosAntecipo.py
+++ trabalho/errosAntecipo.py
@@ -3,6 +3,7 @@ import json
 #mudar o nome do arquivo JSON, para ser carregado pelo python
 with open("antecipo5122.json", encoding='utf-8') as meu_JSON:
     dados = json.load(meu_JSON)
+    
 
 print('\n'
       '*****************************************************************************\n'
@@ -17,17 +18,13 @@ for i in range(numErros):
     numeroCCbs = input("Digite a CCB: ")
     listaErros.append(numeroCCbs)
 
-print(listaErros)
 contador = 0
-for i in range(dados):  
-  if listaErros[i] == i['number']:
-    
-    contador += 1
-    
+for i in dados:  
+  if listaErros[i] == i['number']:    
+    contador += 1    
     numCCB = i['number']
     nome = i['issuer_name']
-    numCPF = i['issuer_cpf']
-    
+    numCPF = i['issuer_cpf']    
     print(f'Erro n: {contador}\nCCB: {numCCB} \nNome: {nome}')
     print('CPF: ' + numCPF[3:6]+'.'+numCPF[6:9]+'.'+numCPF[9:12]+'-'+numCPF[12:14])
     
