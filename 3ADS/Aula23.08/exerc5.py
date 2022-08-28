c57e6fab13e05afb81ea11377d69bbed29570f36
mantiniDev <pmantinidev@gmail.com>
2022-08-28 13:48:25 -0300

    exercFeitos

---
+++ 3ADS/Aula23.08/exerc5.py
@@ -0,0 +1,9 @@
+#area de um trapezio
+
+baseMenor = int(input('Insira a base menor do trapézio: '))
+baseMaior = int(input('Insira a base maior do trapézio: '))
+alturaTrapezio = int(input('Insira a altura do trapézio: '))
+
+respArea = (baseMenor+baseMaior)*alturaTrapezio/2
+
+print(f'A area do trapézio é de {respArea}')
---
+++ 3ADS/Aula23.08/exerc6.py
@@ -0,0 +1,9 @@
+#area de um circulo
+
+import math
+
+raio = int(input('Insira o raio do circulo: '))
+
+resp = math.pi*raio**2
+
+print(f'A area do circulo é {resp}.')
