1b9d3cdf77b47ab5861d857e68dfa91307bf2d75
mantiniDev <pmantinidev@gmail.com>
2022-09-06 20:10:16 -0300

    exercs

---
+++ 4ADS/aula06.09/exerc1.py
@@ -0,0 +1,11 @@
+import emoji
+
+print(emoji.emojize('Eu estou de :eyes: em você.'))
+print(emoji.emojize('Quero \U0001F48B sua boca.'))
+print(emoji.emojize('Meu \U0001F9E1 bate forte por você.'))
+print(emoji.emojize('Está na ota de beber um :coffee.'))
+print(emoji.emojize('Eu estou de :eyes: em você.'))
+print(emoji.emojize('Eu estou de :eyes: em você.'))
+print(emoji.emojize('Eu estou de :eyes: em você.'))
+print(emoji.emojize('Eu estou de :eyes: em você.'))
+print(emoji.emojize('Eu estou de :eyes: em você.'))
