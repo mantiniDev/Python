684afec46ed3a225aada8e19a20c61b2b3bc9bcd
mantiniDev <pmantinidev@gmail.com>
2022-09-06 19:09:44 -0300

    aula06_teoria

---
+++ 4ADS/aula06.09/bibliotecas.py
@@ -0,0 +1,10 @@
+import math
+#from math import sqrt,sin,cos (outro exemplo de ipmportar)
+#ceil - arredondamento para cima
+#floor - arredondamento para baixo
+
+#import emoji - print(emoji.eojize("Ola mundo: earth_aericas: ", use_aliases=True))
+
+#padrão ANSI
+#\033[style, text, back\033[m  ex: 
+print('\033[1;31mPaulo\033[m \033[3;34;41mPaulo\033[m')
