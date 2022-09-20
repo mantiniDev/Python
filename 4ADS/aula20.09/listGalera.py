cdbe437d033818e2b9bd2f02fc5c6924510f0557
mantiniDev <pmantinidev@gmail.com>
2022-09-20 20:22:15 -0300

    aula20.09

---
+++ 4ADS/aula20.09/listGalera.py
@@ -0,0 +1,14 @@
+pessoa = list()
+
+pessoa.append('paulo')
+pessoa.append(18)
+
+galera = list()
+
+
+galera.append('thiago')
+galera.append(40)
+#pessoa[0]= 'thiago'
+#pessoa[1]= 40
+galera.append(pessoa)
+print(galera)
---
+++ 4ADS/aula20.09/lsta_pop_remove.py
@@ -0,0 +1,9 @@
+lista = [1, 3, 4, 5]
+
+x = lista.pop(3)
+y = lista.remove(4)
+
+print(lista)
+print(x)
+print(y)
+
