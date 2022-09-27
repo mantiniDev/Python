c0825bdfc90daa6b7a61cdbc29dd6eed3bfd91af
mantiniDev <pmantinidev@gmail.com>
2022-09-27 20:34:50 -0300

    aula27.09

---
+++ 4ADS/aula27.09/eleicao.py
@@ -0,0 +1,16 @@
+candidatos = { '1': {'Nome': 'Antonio', 'Voto': 0},
+               '2':{'Nome': 'Maria', 'Voto': 0},
+               '3':{'Nome': 'Antonio','Voto': 0},
+               '4':{'Nome': 'Nulo', 'Voto': 0}}
+      
+import os
+
+while true:
+    os.system('CLS')
+    print(f'[1] Antonio\n'
+          f'[2] José\n'
+          f'[3] Maria\n'
+          f'[4] Antonio\n'
+          f'[] Antonio\n')    
+    
+
