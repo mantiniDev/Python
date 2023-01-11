46e1ed6f43bd6ae833cc0cfcf31bd78b57a15fc3
mantiniDev <pmantinidev@gmail.com>
2023-01-11 16:24:23 -0300

    array

--- livro/Rad_python/cap1/array.py
+++ livro/Rad_python/cap1/array.py
@@ -2,11 +2,16 @@
 # uma dimensão - unidimensionais - envolve somente linhas;
 # duas dimensões - bidimensionais - envolvem linhas e colunas
 
-x = []
+x = [0 for i in range(5)]
 
 for i in range(5):
+    x[i] = i + 1
+    print(x[i])
+    i+=1
     
-    
+for i in range(5):
+    print("Digite o numero %s de 5 \n" %(i+1))
+    x[i] = input()
 
 for i in range(5):
     print(x[i])
