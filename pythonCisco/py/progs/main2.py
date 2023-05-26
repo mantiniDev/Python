3a9b0937c4d01b41715a8bb4be995c0364cce2c3
mantiniDev <pmantinidev@gmail.com>
2023-05-26 00:20:59 -0300

    cisco 16.0.23

---
+++ pythonCisco/py/progs/main2.py
@@ -0,0 +1,14 @@
+from sys import path
+
+path.append('E:\\projetoPython\\pythonCisco\\py\\packages\\extra')
+
+import extra.good.best.sigma as sig
+import extra.good.alpha as alp
+
+print(sig.funS())
+print(alp.funA())
+
+
+
+
+
---
+++ pythonCisco/py/teste.py
@@ -0,0 +1,4 @@
+import math
+
+rep = math.e != math.pow(2, 4)
+print(int(rep))
