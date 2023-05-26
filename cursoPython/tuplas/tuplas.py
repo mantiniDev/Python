f67adbcc34547b18ebe80806c8f9bb60c4441190
mantiniDev <pmantinidev@gmail.com>
2023-05-25 22:03:23 -0300

    tuplas

---
+++ cursoPython/tuplas/tuplas.py
@@ -0,0 +1,13 @@
+tupla = ('lira', '25/08/2010', '15/02/1994', 200, 'Estagiário')
+
+#nome = tupla [0] 
+#data_contratacao = tupla [1] 
+#data_nascimento = tupla [2] 
+#salario = tupla [3] 
+#cargo = tupla [4] 
+
+nome, data_cont, data_nasc, sal, cargo = tupla
+
+vendas = [1000, 2000, 300, 300, 150]
+funcionarios = ['joão', 'lira', 'ana', 'Maria', 'Paula']
+print(nome)
