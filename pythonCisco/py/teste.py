25167f5dc5c6db88f27415df7497ed1fda5a6c5d
mantiniDev <pmantinidev@gmail.com>
2023-05-27 01:26:04 -0300

    modulo 2 ok

--- pythonCisco/py/teste.py
+++ pythonCisco/py/teste.py
@@ -1,4 +1,16 @@
-import math
+try:
+    print((""/0))
+except ArithmeticError:
+    print("arith")
+except ZeroDivisionError:
+    print("zero")
+except:
+    print("some")
+    
+print(3 * 'abc' + 'xyz')
 
-rep = math.e != math.pow(2, 4)
-print(int(rep))
+print(ord('c') - ord('a'))
+
+print(chr(ord('z')-2))
+
+assert var == 0
