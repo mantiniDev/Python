d213ef4866e28d381b0da10d3f38b988c569363c
mantiniDev <pmantinidev@gmail.com>
2023-05-31 00:37:37 -0300

    curso cisco

---
+++ pythonCisco/orientacaoObjeto/35.py
@@ -0,0 +1,8 @@
+def fun(n):
+    s = ''
+    for i  in range(n):
+        s += '*'
+        yield s 
+        
+for x in fun(3):
+    print(x, end='')
---
+++ pythonCisco/orientacaoObjeto/44.py
@@ -0,0 +1,5 @@
+def fun(d, k, v):
+    d[k] = v
+
+s = {}
+print(fun(s, '1','v'))
